ccdd6f99df9fb87c97a83a2267fe13308750b353
LucaFerreli <luca.ferreli@users.noreply.github.com>
2020-09-24 09:15:56 +0200

    Anagrafica
    .

---
+++ AnagraficApp/AnagraficApp/Studente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagraficApp
+{
+    class Studente
+    {
+        private string nome;
+        private string cognome;
+        private string luogoNascita;
+        private DateTime dataNascita;
+        private string codiceFiscale;
+        private string classe;
+
+        public Studente(string nome, string cognome, string luogoNascita, DateTime dataNascita, string codiceFiscale, string classe)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.luogoNascita = luogoNascita;
+            this.dataNascita = dataNascita;
+            this.codiceFiscale = codiceFiscale;
+            this.classe = classe;
+        }
+    }
+}
